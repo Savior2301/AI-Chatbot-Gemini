3e31612012b67b3855cf8cbe36c7c598172bc468
Sudhanshu Singh <singhanshujan@gmail.com>
2025-04-17 01:26:47 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useRef, useEffect, useState } from "react";
+import Chatbotform from "./Components/chatbotform";
+import ChatbotIcon from "./Components/chatbotIcon";
+import ChatMessage from "./Components/ChatMessage";
+
+
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
+
+  const generateBotResponse = async (history) => {
+    const updateHistory = (text, isError = false) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."), {role: "model", text, isError}]); 
+    };
+
+
+    history = history.map(({role, text}) => ({role, parts: [{text}]}));
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({contents: history})
+    }
+    try {
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message || "Something went wrong");
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponseText);
+
+    } catch(error) {
+      updateHistory(error.message, true);
+
+    }
+  };
+
+  useEffect(() => {
+    chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight, behavior: "smooth"});
+  }, [chatHistory]);
+
+  return (
+      <div className={`container ${showChatbot ? "show-chatbot" : ""}`}>
+        <button onClick={() => setShowChatbot(prev => !prev)} id="chatbot-toggler">
+          <span className="material-symbols-rounded">mode_comment</span>
+          <span className="material-symbols-rounded">close</span>
+        </button>
+
+        <div className="chatbot-popup">
+          <div className="chat-header">
+            <div className="header-info">
+            <ChatbotIcon />
+              <h2 className="logo-text">Chatbot</h2>
+            </div>
+            <button onClick={() => setShowChatbot(prev => !prev)} className="material-symbols-rounded">keyboard_arrow_down</button>
+          </div>
+          <div ref={chatBodyRef} className="chat-body">
+            <div className="message bot-message">
+            <ChatbotIcon />
+              <p className="message-text">
+                Hey there 👋 <br /> How can I help you?
+              </p>
+            </div>
+
+            {/* render the chat history dynamically */}
+            {chatHistory.map((chat, index) => (
+              <ChatMessage key={index} chat={chat} />
+            ))}
+          </div>
+          <div className="chat-footer">
+            <Chatbotform chatHistory = {chatHistory} setChatHistory = {setChatHistory} generateBotResponse = {generateBotResponse} />
+          </div>
+        </div>
+      </div>
+  )
+}
+export default App;
